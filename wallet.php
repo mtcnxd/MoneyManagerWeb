b2e0a0c4396d0c48c21dc7c1881413fb0a937526
Marcos Tzuc <marcos@virtualia.com>
2022-09-09 18:10:04 -0500

    Agregando componentes nuevos de prueba

--- wallet.php
+++ wallet.php
@@ -105,6 +105,16 @@ if($_POST){
 						<div class="card-body">
 							<h6 class="card-title">Lista de movimientos del dia</h6>
 							<p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+							<div class="btn-group btn-group-sm" role="group" aria-label="Small button group">
+								<button type="button" class="btn btn-outline-secondary hstack gap-2">
+									<svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" viewBox="0 0 24 24" fill="none" stroke="#555555" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-bell"><path d="M18 8A6 6 0 0 0 6 8c0 7-3 9-3 9h18s-3-2-3-9"></path><path d="M13.73 21a2 2 0 0 1-3.46 0"></path></svg>
+									Notificaciones
+								</button>
+								<button type="button" class="btn btn-outline-secondary hstack gap-2">
+								<svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" viewBox="0 0 24 24" fill="none" stroke="#555555" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-file-plus"><path d="M14 2H6a2 2 0 0 0-2 2v16a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V8z"></path><polyline points="14 2 14 8 20 8"></polyline><line x1="12" y1="18" x2="12" y2="12"></line><line x1="9" y1="15" x2="15" y2="15"></line></svg>
+									Agregar nuevo
+								</button>
+							</div>
 						</div>
 					</div> <!-- Card -->
 
@@ -128,9 +138,15 @@ if($_POST){
 									echo "</tr>";
 								}
 								?>
+								<small class="d-inline-flex mb-3 px-2 py-1 fw-semibold text-success bg-success bg-opacity-10 border border-success border-opacity-10 rounded-2">Added in v5.2.0</small>
 							</table>
 						</div>	
-					</div> 	<!-- Card -->					
+					</div> 	<!-- Card -->
+					
+					<div class="alert alert-warning alert-custom" role="alert">
+						<h6>Titulo del alert</h6>
+						A simple warning alert—check it out!
+					</div>
 
 				</div>	<!-- Col -->
 
