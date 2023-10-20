129f8f98b0de75ffcf2293a68cc46f032a839598
Marcos Tzuc Cen <marcos@virtualia.com>
2023-10-20 15:09:30 -0600

    include meta_head

--- calendar.php
+++ calendar.php
@@ -11,20 +11,7 @@ $wallet = new myWallet();
 
 <html>
 	<head>
-		<title>My Wallet</title>
-		<meta name="viewport" content="width=device-width, initial-scale=1">
-		<!-- CSS only -->
-		<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/css/bootstrap.min.css" 
-				rel="stylesheet" 
-				integrity="sha384-KyZXEAg3QhqLMpG8r+8fhAXLRk2vvoC2f3B09zVXn8CA5QIVfZOJ3BCsw2P0p/We" 
-				crossorigin="anonymous">
-		<link href="css/custom.css" rel="stylesheet"/>
-				
-		<!-- JavaScript Bundle with Popper -->
-		<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/js/bootstrap.bundle.min.js" 
-				integrity="sha384-U1DAWAznBHeqEIlVSCgzq+c9gqGAJn5c/t99JyeKa9xxaYpSvHU5awsuZVVFIhvj" 
-				crossorigin="anonymous">
-		</script>		
+		<?php include ('includes/meta_head.php'); ?>
 	</head>
 
 	<body>
