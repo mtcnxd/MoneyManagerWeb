70f5d21c3c4abbbda8f23966bc0d5217544be37e
Marcos Tzuc <mtc.nxd@gmail.com>
2024-10-29 11:55:09 -0600

    Adding button to delete transaction in details

--- details.php
+++ details.php
@@ -35,6 +35,7 @@ $investments = new investments();
 										<th scope="col">Instrumento</td>
 										<th scope="col">Fecha</td>
 										<th scope="col" class="text-end">Saldo</td>
+										<th scope="col" class="text-end"></td>
 									</tr>
 								</thead>
 								<?php
@@ -49,6 +50,11 @@ $investments = new investments();
 									echo "	<td>". $invest->category ."</td>";
                                     echo "	<td>". $dateTime->format('d-m-Y') ."</td>";
 									echo "	<td class='text-end'> $". number_format($invest->amount, 2) ."</td>";
+									echo '  <td>
+												<a href="#" id='.$invest->id.' class="btn-delete">
+												<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="#555555" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-x-square"><rect x="3" y="3" width="18" height="18" rx="2" ry="2"></rect><line x1="9" y1="9" x2="15" y2="15"></line><line x1="15" y1="9" x2="9" y2="15"></line></svg>
+												</a>
+											</td>';
 									echo "</tr>";
 								}
 
