0d11374c1d7e014bd6f1f8623be45655241334e7
Marcos Tzuc <mtc.nxd@gmail.com>
2024-11-14 15:39:01 -0600

    Exec query when receive category

--- api/alexa.php
+++ api/alexa.php
@@ -3,12 +3,13 @@
 require_once ('../classes/QueryBuilder.php');
 
 header('Content-Type: application/json');
+
 $json = file_get_contents("php://input");
 $data = json_decode($json, true);
 
 $query = "SELECT MAX(AMOUNT) amount FROM wallet_invest a ".
 "JOIN wallet_categories b on a.category_id = b.id ".
-"WHERE b.category = '".$data->category."'";
+"WHERE b.category = '".$data['category']."'";
 
 $mysql   = new QueryBuilder();
 $results = $mysql->get($query);
